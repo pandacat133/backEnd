89abc4fc430b29caeeaa6572dd2ffa534309faf2
Celie Baker <celie.n133@gmail.com>
2018-08-23 10:00:12 -0600

    stuff

---
+++ day8/api.js
@@ -0,0 +1,6 @@
+const http = require('http');
+const data = require('./data/inventory');
+
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content t'})
+})
--- day8/httpServer.js
+++ day8/httpServer.js
@@ -1,22 +1,30 @@
 let http = require('http');
+const fs = require('fs');
 
 let server = http.createServer((req, res) => {
 
-    res.writeHead(200, {'Content-type': 'text/html'});
-    res.end(`
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <title>HTML Response</title>
-            </head>
-            <body>
-                <h1>Serving HTML</h1>
-                <p>${req.url}</p>
-                <p>${req.method}</p>
-            </body>
-        </html>
-    `);
-
+    if(req.url === '/') {
+        fs.readFile('./public/index.html', 'utf8', (err, html) => {
+            res.writeHead(200, {'Content-type': 'text/html'});
+            res.end(html);
+        });
+    }
+    else if(req.url.match(/.css$/)){
+        let cssPath = path.join(__dirname, 'public', req.url);
+        let fileStream = fs.createReadStream(cssPath, 'utf8');
+        res.writeHead(200, {'Content-type': 'text/css'});
+        fileStream.pipe(res);
+    }
+    else if(req.url.match(/.jpg$/)){
+        let imgPath = path.join(__dirname, 'public', req.url);
+        let imgStream = fs.createReadStream(imgPath);
+        res.writeHead(200, {'Content-type': 'image/jpg'})
+        imgStream.pipe(res);
+    }
+    else{
+        res.writeHead(404, {'Content-type':'text/plain'});
+        res.end('404 file not found');
+    }
 }).listen(3000);
 
 console.log(`listening on port ${server.address().port}`);
