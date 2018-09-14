0d15f43405af0c8b75b22e37e39cbcaac7343fe1
Celie Baker <celie.n133@gmail.com>
2018-09-14 09:30:51 -0600

    stuff

--- day21/app.js
+++ day21/app.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/userManagement', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost/userManagementDB', {useNewUrlParser: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
     console.log('db connected');
 });
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    role: String,
+    age: { type: Number, min: 18, max: 70 },
+    createdData: { type: Date, default: Date.now}
+});
+
+const user = mongoose.model('userCollection', userSchema);
