f39d93ab41a5d5da729e33b15bb439c930ea9c34
Celie Baker <celie.n133@gmail.com>
2018-09-18 09:36:13 -0600

    stuff

--- day21/app.js
+++ day21/app.js
@@ -21,6 +21,57 @@ app.listen(port, (err) => {
     console.log(`App Server listen on port: ${port}`);
 });
 
+app.post('/newUser', (req, res) => {
+    console.log(`POST /newUser: ${JSON.stringify(req.body)}`);
+    const newUser = new user();
+    newUser.name = req.body.name;
+    newUser.role = req.body.role;
+    newUser.save((err, data) => {
+        if (err) {
+            return console.error(err);
+        }
+        console.log(`new user save: ${data}`);
+        res.send(`done ${data}`);
+    });
+});
+
+app.post('/updateUserRole', (req, res) => {
+    console.log(`POST /updateUserRole: ${JSON.stringify(req.body)}`);
+    let matchedName = req.body.name;
+    let newRole = req.body.role;
+    user.findOneAndUpdate( { name: matchedName }, { role: newRole }, { new: true }, (err, data) => {
+        if (err) return console.log(`Oops! ${err}`);
+        console.log(`data -- ${data.role}`);
+        let returnMsg = `user name : ${matchedName} new role: ${data.role}`;
+        console.log(returnMsg);
+        res.send(returnMsg);
+    });
+});
+
+app.post('/removeUser', (req, res) => {
+    console.log(`POST /removeUser: ${JSON.stringify(req.body)}`);
+    let matchedName = req.body.name;
+    user.findOneAndDelete( { name: matchedName }, (err, data) => {
+        if (err) return console.log(`Oops! ${err}`);
+        console.log(`data -- ${data.role}`);
+        let returnMsg = `user name : ${matchedName} removed data: ${data}`;
+        console.log(returnMsg);
+        res.send(returnMsg);
+    });
+});
+
+app.get('/user/:name', (req, res) => {
+    let userName = req.params.name;
+    console.log(`GET /user/:name: ${JSON.stringify(req.params)}`);
+    user.findOne({ name: userName }, (err, data) => {
+        if (err) return console.log(`Oops! ${err}`);
+        console.log(`data -- ${JSON.stringify(data)}`);
+        let returnMsg = `user name : ${userName} role : ${data.role}`;
+        console.log(returnMsg);
+        res.send(returnMsg);
+    });
+});
+
 mongoose.connect('mongodb://localhost/userManagementDB', {useNewUrlParser: true});
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
